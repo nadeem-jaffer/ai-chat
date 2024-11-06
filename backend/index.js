55d793ac32fcbbc39a4e2421a39a64ce07334156
nadeem-jaffer <moham156@pnw.edu>
2024-11-06 10:52:19 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,18 +6,23 @@ const dotenv = require("dotenv");
 const authRoute = require("./Routes/store")
 const cors = require("cors");
 
-dotenv.config();
 
 const app = express();
-app.use(cors()); 
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+
+const PORT = 5000;
+app.use("/api/store", authRoute);
 
 mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => console.log("DB Connected"))
-  .catch((err) => console.log(err));
+  .connect("mongodb://localhost:27017/yourDatabaseName", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.error("MongoDB connection error:", error));
 
-app.use("/api/store",authRoute)
-
-app.listen(8800, () => {
-  console.log("Backend is running");
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
