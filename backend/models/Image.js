763ce4df1e4d411023e1eddc0a9631b79a1191aa
nadeem-jaffer <moham156@pnw.edu>
2024-11-13 22:14:48 +0530

    Update Image.js

--- backend/models/Image.js
+++ backend/models/Image.js
@@ -1,10 +1,11 @@
+
+
 const mongoose = require("mongoose");
 
 const ImageSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name:{type:String,required: true},
   prompt: { type: String, required: true },
-  imgData: { type: Buffer, required: true }, // Store image as Buffer
-  contentType: { type: String, required: true }, // Store content type of the image
+  imgUrl: { type: String, required: true, unique: true },
 });
 
 module.exports = mongoose.model("Image", ImageSchema);
